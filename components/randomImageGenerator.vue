7c27e780e4049dd24f186ef64a9b501893f23df4
WhoopsImTop <eliasenglen2@gmail.com>
2025-02-20 09:52:50 +0100

    add new

--- components/randomImageGenerator.vue
+++ components/randomImageGenerator.vue
@@ -2,11 +2,22 @@
   <div
     class="flex justify-center items-center relative rounded-xl overflow-hidden"
   >
-    <img
-      class="object-cover w-full h-full"
-      :src="landingImage.image"
-      alt="landing-image"
-    />
+    <transition name="fade" mode="out-in" class="z-10">
+      <img
+        :key="landingImage.image"
+        class="object-cover w-full h-full"
+        :src="landingImage.image"
+        alt="landing-image"
+      />
+    </transition>
+    <transition v-if="prevImage.image" name="fade" mode="out-in">
+      <img
+        :key="prevImage.image"
+        class="object-cover w-full h-full absolute top-0 left-0 right-0 bottom-0"
+        :src="prevImage.image"
+        alt="landing-image"
+      />
+    </transition>
     <div
       v-if="landingImage?.copyright"
       class="absolute px-3 pt-1 pb-2 rounded-tr-md bg-black/70"
@@ -34,12 +45,31 @@ import landing01 from '../static/1.jpg'
 import landing02 from '../static/2.jpg'
 import landing03 from '../static/3.jpg'
 import landing04 from '../static/4.jpg'
-import landing05 from '../static/5.jpg'
-import landing06 from '../static/6.jpg'
-import landing07 from '../static/7.jpg'
-import landing08 from '../static/8.jpg'
+import landing05 from '../static/5.JPG'
+import landing06 from '../static/6.JPG'
+import landing07 from '../static/7.JPG'
+import landing08 from '../static/8.JPG'
 import landing09 from '../static/9.jpg'
 import landing10 from '../static/10.jpg'
+import landing11 from '../static/11.jpeg'
+import landing12 from '../static/12.jpeg'
+import landing13 from '../static/13.jpeg'
+import landing14 from '../static/14.jpeg'
+import landing15 from '../static/15.jpeg'
+import landing16 from '../static/16.jpeg'
+import landing17 from '../static/17.jpeg'
+import landing18 from '../static/18.jpeg'
+import landing19 from '../static/19.jpeg'
+import landing20 from '../static/20.jpeg'
+import landing21 from '../static/21.jpeg'
+import landing22 from '../static/22.jpeg'
+import landing23 from '../static/23.jpeg'
+import landing24 from '../static/24.jpeg'
+import landing25 from '../static/25.jpeg'
+import landing26 from '../static/26.jpeg'
+import landing27 from '../static/27.jpeg'
+import landing28 from '../static/28.jpg'
+import landing29 from '../static/29.jpg'
 export default {
   props: {
     translations: {
@@ -96,12 +126,90 @@ export default {
           image: landing10,
           copyright: 'Patricia Witzigmann',
         },
+        {
+          image: landing11,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing12,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing13,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing14,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing15,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing16,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing17,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing18,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing19,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing20,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing21,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing22,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing23,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing24,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing25,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing26,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing27,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing28,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
+        {
+          image: landing29,
+          copyright: 'Hamburger Gästeführer e.V.',
+        },
       ],
       landingImage: {},
+      prevImage: {},
     }
   },
-  mounted() {
+  beforeMount() {
     this.getRandomImage()
+    this.changeImages()
   },
   methods: {
     getRandomImage() {
@@ -109,13 +217,31 @@ export default {
         this.getRandomImage()
       } else {
         this.landingImage = this.chooseRandomImage()
+        this.prevImage = this.landingImage
       }
     },
     chooseRandomImage() {
       return this.images[Math.floor(Math.random() * this.images.length)]
     },
+    changeImages() {
+      setInterval(() => {
+        this.getRandomImage()
+        setTimeout(() => {
+          this.prevImage = this.landingImage
+        }, 500)
+      }, 8000)
+    },
   },
 }
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease-in-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
